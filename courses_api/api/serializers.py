ade4cf6532c642ec41ef0104e37f7bbb95bcf4c8
Andriy <146370343+sixoper77@users.noreply.github.com>
2025-11-13 22:07:28 +0200

    fix login

--- courses_api/api/serializers.py
+++ courses_api/api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from . models import Course,User
 from django.db.models import Avg
+from django.contrib.auth.password_validation import validate_password
+from .validators import CourseUniqueValidator
+from django.contrib.auth import authenticate
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,7 +13,6 @@ class UserSerializer(serializers.ModelSerializer):
                 'first_name',
                 'last_name',
                 'id')
-
 
 class ShortCourseSerializer(serializers.ModelSerializer):
     teacher=UserSerializer(read_only=True)
@@ -23,3 +25,62 @@ class ShortCourseSerializer(serializers.ModelSerializer):
     def get_average_rating(self,obj):
         avg=obj.avg_rating
         return round(avg,2) if avg else 0
+    
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True,
+                                   validators=[validate_password])
+    password_confirmed=serializers.CharField(write_only=True)
+    email=serializers.EmailField(validators=[CourseUniqueValidator(User,"email")])
+    username=serializers.CharField(validators=[CourseUniqueValidator(User,"username")])
+    is_teacher=serializers.BooleanField(default=False)
+    
+    class Meta:
+        model=User
+        fields=('username','first_name','last_name','password','is_teacher','password_confirmed','email')
+        
+    def validate(self, attrs):
+        if attrs['password']!=attrs['password_confirmed']:
+            raise serializers.ValidationError({
+                "password":"passwords don't match"
+            })
+        return attrs
+    def create(self, validated_data):
+        validated_data.pop('password_confirmed')
+        user=User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name',''),
+            last_name=validated_data.get('last_name',''),
+            password=validated_data['password'],
+            is_teacher=validated_data.get('is_teacher',False)
+        )
+        return user
+    
+class UserLogInSerializer(serializers.Serializer):
+    password=serializers.CharField(write_only=True)
+    username=serializers.CharField()
+    
+    def validate(self, attrs):
+        password=attrs.get('password')
+        username=attrs.get('username')
+        if password and username:
+            user=authenticate(
+                request=self.context.get('request'),
+                username=username,
+                password=password
+            )
+            
+            if not user:
+                raise serializers.ValidationError('User not found.')
+            
+            if not user.is_active:
+                raise serializers.ValidationError('User account is disabled')
+            attrs['user']=user
+            return attrs
+        else:
+            raise serializers.ValidationError('Must include email and password')
+    
+            
+    
+    
